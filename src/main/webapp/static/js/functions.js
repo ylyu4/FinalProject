987a6cd803a8ebeb7f1758da85823ecf5a6b1b9c
Xu Huang <huangxu@gwu.edu>
2021-03-22 17:34:50 +0800

    CSS: freelancer_remuneration_history.html

--- src/main/webapp/static/js/functions.js
+++ src/main/webapp/static/js/functions.js
@@ -1,3 +1,12 @@
 function buttonJump(destination){
     window.location.href=destination;
 }
+
+function freelancerViewResumeEditEnable(){
+  document.getElementById("freelancerEditEEFrom"). disabled = "";
+  document.getElementById("freelancerEditEETo").disabled = "";
+  document.getElementById("freelancerEditEESchool").disabled = "";
+  document.getElementById("freelancerEditEEDegree"). disabled = "";
+  document.getElementById("freelancerEditEEMajor").disabled = "";
+  document.getElementById("freelancerEditEEDescription").disabled = "";
+}
